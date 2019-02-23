1f92c3e1eb2f8f5b310de99642b3d344eae1d34a
Miguel Fernandez <miguel.fernandez@codeimmersives.com>
2019-02-23 16:48:02 -0500

    Fixed Capitalized, still have a comma

--- main.js
+++ main.js
@@ -4,10 +4,26 @@ function init() {
     document.querySelector('#input')
         .addEventListener('keyup', handleTyping);
 }
-
+// we want to get the input value
+// we want to take the first letter of every word to be capitalized
+// any letter after whitespace to be capitalized
+// use a loop to find the first letter in a word
+// the loop stops when there's no whitespace;
 function handleTyping(event) {
-    let userInput = event.target.value;
-    let answer = userInput;
+    let userInput = event.target.value.toString( );
+    let answer = userInput.split(' ');
+
+    // Codes
+    let i = 0;
+
+while (i < answer.length) {
     
-    document.querySelector('#changeme').innerText = answer;
+    answer[i] = answer[i].charAt(0).toUpperCase() + answer[i].slice(1).toLowerCase();
+    
+    i++;
 }
+
+    document.querySelector('#result').innerText = answer;
+}
+
+
